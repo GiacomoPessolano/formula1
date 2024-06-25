cf5b63b7077e7325fa8a70dce7028012fe90ae5c
Giacomo Pessolano <giacomo020803gmail.com>
2024-06-25 14:14:14 +0200

    Retouched UICLI and made Main class; PROBLEM: GRADLE WON'T RUN PROJECT

---
+++ app/src/main/java/it/unicam/cs/giacomopessolano/formula1/exceptions/IncorrectConfigurationException.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.giacomopessolano.formula1.exceptions;
+
+import java.io.IOException;
+
+public class IncorrectConfigurationException extends IOException {
+
+    public IncorrectConfigurationException(String message) {
+        super(message);
+    }
+
+}
---
+++ app/src/main/java/it/unicam/cs/giacomopessolano/formula1/exceptions/UnrecognizedFileException.java
@@ -0,0 +1,11 @@
+package it.unicam.cs.giacomopessolano.formula1.exceptions;
+
+import java.io.IOException;
+
+public class UnrecognizedFileException extends IOException {
+
+    public UnrecognizedFileException(String message) {
+        super(message);
+    }
+
+}
